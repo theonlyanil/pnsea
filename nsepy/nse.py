83f35721e1c2f873c382602637ddbf3e40870853
theonlyanil <theonlyanil@gmail.com>
2025-02-14 20:56:19 +0530

    nsepy init

---
+++ nsepy/nse.py
@@ -0,0 +1,84 @@
+from nsesession import NSESession
+
+class NSE:
+    def __init__(self):
+        self.session = NSESession()
+    """
+    Get Insider Data for a single company
+    
+    Input: 
+    symbol: str: Symbol of the company
+    
+    """
+    def insider_data(self, symbol, from_date=None, to_date=None):
+        url = f"https://www.nseindia.com/api/corporates-pit?index=equities&symbol={symbol}"
+        params = dict()
+        if from_date:
+            params["from_date"] = from_date
+        if to_date:
+            params["to_date"] = to_date
+
+        response = self.session.get(url, params=params)
+
+        return response.json()
+    
+    """
+    Get Insider Data for a single company
+
+    Input:
+    start_date: str: Start Date in dd-mm-yyyy format
+    end_date: str: End Date in dd-mm-yyyy format
+    symbol: str: Symbol of the company
+
+    """
+    def single_co_insider_data(self, start_date, end_date, symbol):
+        params = dict()
+        params["symbol"] = symbol
+        params["from_date"] = start_date
+        params["to_date"] = end_date
+        url = 'https://www.nseindia.com/api/corporates-pit?index=equities'
+        response = self.session.get(url, params=params)
+        return response.json()
+    
+
+    """
+    Get Pledged Data for a single company
+    """
+    def getPledgedData(self, symbol):
+        url = f"https://www.nseindia.com/api/corporate-pledgedata?index=equities&symbol={symbol}"
+        response = self.session.get(url)
+        return response.json()
+    
+    def getSastData(self, symbol):
+        url = "https://www.nseindia.com/api/corporate-sast-reg29"
+        params = dict()
+        params["index"] = 'equities'
+        params["symbol"] = symbol
+
+        response = self.session.get(url, params=params)
+        return response.json()
+
+
+if __name__ == "__main__":
+    nse = NSE()
+
+    """
+    Insider Data
+    """
+    #print(nse.insider_data("SBIN"))
+    print(nse.insider_data("INFY", from_date="11-12-2023", to_date="14-02-2025"))
+
+    """
+    Single Company Insider Data
+    """
+    #print(nse.single_co_insider_data("11-12-2020", "14-02-2025", "SBIN"))
+
+    """
+    Pledged Data
+    """
+    #print(nse.getPledgedData("SBIN"))
+
+    """
+    Sast Data
+    """
+    #print(nse.getSastData("SBIN"))
